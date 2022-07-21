31d8558db5395be13d50b245d866278bf17878ab
Danilo Santos <danilo@devstudio.com.br>
2022-07-20 21:16:18 -0300

    Criando categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express"; // o router ele nos ajuda a manipular as rotas de forma modular.
+import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    const category: Category = {
+        name,
+        description,
+        id: uuidV4(),
+        created_at: new Date(),
+    };
+
+    categories.push(category);
+
+    return response.status(201).send();
+});
+
+export { categoriesRoutes };
