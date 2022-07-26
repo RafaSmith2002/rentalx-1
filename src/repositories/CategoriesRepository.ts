cd8173419b600bf29c4bf57a4b6d46bc01f6ad43
Danilo Santos <danilo@devstudio.com.br>
2022-07-25 21:17:34 -0300

    criando repositories da categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,32 @@
+import { Category } from "../model/category";
+
+// DTO - Data Transfer Object
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+class CategoriesRepository {
+    private categories: Category[]; // private define como o atributo vai ser acessado.
+
+    constructor() {
+        this.categories = [];
+    }
+    create({ description, name }: ICreateCategoryDTO) {
+        const category = new Category();
+
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.categories.push(category);
+    }
+    // listar cadastros das categorias
+    list(): Category[] {
+        return this.categories;
+    }
+}
+
+export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,21 @@
 import { Router } from "express"; // o router ele nos ajuda a manipular as rotas de forma modular.
-import { v4 as uuidV4 } from "uuid";
 
-import { Category } from "../model/category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category: Category = {
-        name,
-        description,
-        id: uuidV4(),
-        created_at: new Date(),
-    };
-
-    categories.push(category);
+    categoriesRepository.create({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const lista = categoriesRepository.list();
+    return response.json(lista);
 });
 
 export { categoriesRoutes };
